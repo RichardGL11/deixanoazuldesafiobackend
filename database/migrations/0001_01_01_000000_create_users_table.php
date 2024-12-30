99b934069921c1d962def074b21e224ae61dcc35
RichardGL11 <richardgreghilammenda@hotmail.com>
2024-12-30 19:09:32 -0300

    adding amount propety

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('CPF')->unique();
             $table->date('birthdate');
+            $table->float('amount');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
