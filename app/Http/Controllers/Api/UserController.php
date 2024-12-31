71513588fea4fb288aac14ddf1acc8b8a7557e37
RichardGL11 <richardgreghilammenda@hotmail.com>
2024-12-30 22:23:19 -0300

    fixing problems to delete an user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -38,7 +38,7 @@ class UserController extends Controller
 
     public function destroy(User $user): JsonResponse
     {
-        if ($user->wallet->transactions()->count() > 0) {
+        if (!empty($user->wallet->transactions) && $user->wallet->transactions()->count() > 0) {
             return response()->json(['message' => 'User cannot be deleted because it has transactions'], 403);
         }
         $user->delete();
