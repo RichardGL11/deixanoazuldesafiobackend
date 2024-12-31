a9d37efaf3653729f5121b435850dec769e79797
RichardGL11 <richardgreghilammenda@hotmail.com>
2024-12-31 12:29:44 -0300

    creating actions

---
+++ app/Actions/CreateTransactionAction.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Actions;
+
+use App\Http\Requests\CreateTransanctionRequest;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Models\Wallet;
+use Illuminate\Support\Facades\DB;
+
+class CreateTransactionAction
+{
+    public function __construct(public UpdateUserAmountAction $action)
+    {}
+
+    public function execute(CreateTransanctionRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+           $transaction = Transaction::query()->create([
+                'wallet_id' => $request->validated('wallet_id'),
+                'amount'    => (float)$request->validated('amount'),
+                'type'      => $request->validated('type'),
+            ]);
+
+
+            $user = Wallet::query()->findOrFail($request->validated('wallet_id'));
+            $this->action->execute($user->user, $transaction);
+            return response()->json(['message' => 'Transaction Created Successfully'], 200);
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+
+//
+//        DB::transaction(function () use ($request) {
+//            Transaction::query()->create([
+//                'wallet_id' => $request->validated('wallet_id'),
+//                'type'      => $request->validated('type'),
+//                'amount'    => $request->validated('amount'),
+//            ]);
+//            return response()->json(['message' => 'Transaction Created Successfully'], 200);
+//        });
+    }
+}
